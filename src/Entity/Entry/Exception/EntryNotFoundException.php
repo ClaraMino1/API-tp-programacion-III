ad298fc52e3ad9f23fc9fe2ae408df37fb7b5de6
Clara Miño <claraa.mino12@gmail.com>
2025-06-13 20:58:53 -0300

    borrando articles

--- src/Entity/Entry/Exception/EntryNotFoundException.php
+++ src/Entity/Entry/Exception/EntryNotFoundException.php
@@ -6,6 +6,6 @@ use Exception;
 
 final class EntryNotFoundException extends Exception {
     public function __construct(int $id) {
-        parent::__construct("No se encontro el articulo correspondiente. Id: ".$id);
+        parent::__construct("No se encontro la entrada correspondiente. Id: ".$id);
     }
 }
